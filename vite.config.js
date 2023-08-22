1a5d5a988a913451c3d1d3a141547917c2f1180e
Djoult <55453354+Djoult@users.noreply.github.com>
2023-08-22 17:12:09 +0300

    init project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+              @import "./src/styles/_shared.scss";
+            `,
+      },
+    },
+  },
+});
