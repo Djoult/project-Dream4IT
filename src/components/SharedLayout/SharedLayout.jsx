1056e5c783f65921260d4d00918a8d38ccf87321
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-08-24 23:12:19 +0300

    delete test

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Socials from "../Socials/Socials";
 
 const SharedLayout = () => {
   return (
     <>
       <header>Header Component</header>
-      <Socials/>
       <Outlet />
       <footer>Footer Component</footer>
     </>
