a355dc37a6d915a21cf5af3e0204d7071ff620ef
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-08-24 23:04:05 +0300

    adding socials

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Socials from "../Socials/Socials";
 
 const SharedLayout = () => {
   return (
     <>
       <header>Header Component</header>
+      <Socials/>
       <Outlet />
       <footer>Footer Component</footer>
     </>
---
+++ src/components/Socials/Socials.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const SocialsWrapper = styled.div`
+`;
+
+export const SocialsList = styled.ul`
+  display: flex;
+  gap:16px;
+  /* for mobile 14px */
+`;
+
+export const SocialsItem = styled.li`
+`;
+
+export const SocialsLink = styled.a`
+`;
