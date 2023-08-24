62cc56e752043b488bd65e0d7e6758e38c1c26b0
Taras Papka <taras.papka@protonmail.com>
2023-08-24 14:22:02 +0300

    adjusted text for policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import TermsOfServicePage from './pages/TermsOfServicePage/TermsOfServicePage'
 
 
 function App() {
 
   return (
     <>
+      <TermsOfServicePage/>
     </>
   )
 }
