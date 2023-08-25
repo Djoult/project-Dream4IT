97271887d7acad039e09cecc9818934b98cf9ee5
VVladyslav <VVladyslav@users.noreply.github.com>
2023-08-25 21:05:13 +0300

    GlobalStyles colors
    В файлі GlobalStyles.jsx лежить об'єкт colors з кольорами та скиданням стилів. Шрити підключені і index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
+import {GlobalStyles} from './styles/GlobalStyles.js'
 import UserRoutes from './UserRoutes'
 import SubscribeForm from './components/SubscribeForm/SubscribeForm'
-
-
 
 const App = () => {
 
   return (
-    <>
+    <GlobalStyles>
       <UserRoutes />
       <SubscribeForm/>
-    </>
+    </GlobalStyles>
   )
 }
 
