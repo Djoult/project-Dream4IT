1e7dfb5bd997acc6a6498a7c494270189c2db757
Kolesanencore <kolesanencore@gmail.com>
2023-08-25 16:28:22 +0300

    Added welcome page , auth nav and background layout

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,29 @@
+import {
+  TextWrapper,
+  WelcomeDescription,
+  ButtonWrapper,
+  SectionTitle,
+} from "./WelcomePage.styled";
 
+import AuthNav from "../../components/AuthNav/AuthNav";
+import BackgroundLayout from "../../components/BackgroundLayout/BackgroundLayout";
 
 const WelcomePage = () => {
-  return <div>WelcomePage</div>;
+  return (
+    <BackgroundLayout>
+      <TextWrapper>
+        <SectionTitle>Welcome to the app!</SectionTitle>
+        <WelcomeDescription>
+          This app offers more than just a collection of recipes - it is
+          designed to be your very own digital cookbook. You can easily save and
+          retrieve your own recipes at any time.
+        </WelcomeDescription>
+        <ButtonWrapper>
+          <AuthNav />
+        </ButtonWrapper>
+      </TextWrapper>
+    </BackgroundLayout>
+  );
 };
 
 export default WelcomePage;
---
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+export const TextWrapper = styled.div`
+  text-align: left;
+  padding: 273px 855px 376px 100px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SectionTitle = styled.h2`
+  text-align: start;
+  color: #fafafa;
+  font-family: Manrope;
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 44px;
+  letter-spacing: -0.8px;
+`;
+
+export const WelcomeDescription = styled.p`
+  color: #fafafa;
+  font-family: Manrope;
+  font-size: 18px;
+  line-height: 24px;
+  letter-spacing: -0.36px;
+  margin-bottom: 40px;
+  /* margin-top: 14px; */
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 14px;
+  z-index: 200;
+`;
