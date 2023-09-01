c561169a1a4a95f2842fa4a4c40ddde63ac653e8
Kolesanencore <kolesanencore@gmail.com>
2023-09-01 12:52:18 +0300

    Fixed Markup

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -1,33 +1,33 @@
 import styled from "@emotion/styled";
 
 export const WelcomePageContainer = styled.div`
-  width: 375px;
-  font-size: 28px;
+  min-width: 335px;
+
   display: flex;
-  align-items: center;
   flex-direction: column;
+  box-sizing: content-box;
 
   padding: 304px 28px;
 
   @media (min-width: 568px) {
     align-items: baseline;
-    width: 470px;
     padding: 400px 234px 400px 64px;
+    max-width: 400px;
   }
 
   @media (min-width: 768px) {
-    width: 485px;
-    padding: 273px 855px 376px 100px;
+    padding: 273px 855px 273px 100px;
+    max-width: 400px;
   }
 `;
 
 export const SectionTitle = styled.h2`
   color: #fafafa;
 
-  align-items: center;
   text-align: center;
 
-  font-family: Manrope;
+  white-space: nowrap;
+
   font-size: 28px;
   font-weight: 600;
   line-height: 44px;
@@ -35,7 +35,6 @@ export const SectionTitle = styled.h2`
 
   @media (min-width: 568px) {
     text-align: left;
-    width: 393px;
     font-size: 40px;
   }
 `;
@@ -44,28 +43,36 @@ export const WelcomeDescription = styled.p`
   text-align: center;
   color: #fafafa;
 
-  font-family: Manrope;
   font-size: 14px;
   line-height: 24px;
   letter-spacing: -0.36px;
 
   margin-top: 14px;
-
   margin-bottom: 40px;
 
   @media (min-width: 568px) {
+    width: 432px;
+
     text-align: left;
     font-size: 18px;
   }
 
   @media (min-width: 768px) {
+    width: 437px;
+
     text-align: left;
     font-size: 18px;
   }
 `;
 
 export const ButtonWrapper = styled.div`
+  width: 337px;
+  justify-content: center;
   display: flex;
+
   gap: 14px;
-  z-index: 200;
+
+  @media (min-width: 568px) {
+    justify-content: start;
+  }
 `;
