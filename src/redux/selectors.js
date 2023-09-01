86ab4d393500fc51e0c042642822dbf2fe043868
Taras Papka <taras.papka@protonmail.com>
2023-09-01 21:52:42 +0300

    drinks page

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,3 @@
 export const selectUserNeedVerification = state => state.auth.userNeedVerification;
 export const selectIsLoading = state => state.auth.isLoading;
 export const selectCurrentUser = state => state.auth.currentUser;
-
