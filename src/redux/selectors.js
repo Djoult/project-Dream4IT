11d5987f4ecf6e4514a694cd4f25305a6bb8a068
Taras Papka <taras.papka@protonmail.com>
2023-09-01 21:47:29 +0300

    drinks page

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,16 +1,4 @@
-import { createSelector } from "@reduxjs/toolkit";
-
 export const selectUserNeedVerification = state => state.auth.userNeedVerification;
 export const selectIsLoading = state => state.auth.isLoading;
+export const selectCurrentUser = state => state.auth.currentUser;
 
-//drinks page
-export const getRecipes = state => state.recipes.items;
-export const getFilter = state => state.filter;
-export const filteredContacts = createSelector(
-  [getRecipes, getFilter],
-  (recipes, filter) => {
-    return recipes.filter(({ name }) =>
-      name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
-);
