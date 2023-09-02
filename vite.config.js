889bf8225ee04f3d2f03a50a544e8d0355aad1fd
dusixx <113414935+dusixx@users.noreply.github.com>
2023-09-02 20:33:24 +0300

    small fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/project-Dream4IT/",
+  base: '/project-Dream4IT',
   css: {
     preprocessorOptions: {
       scss: {
