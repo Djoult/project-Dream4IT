f5089667adeb3b165d1cd5e270f4d4ffa6e2c31b
Djoult <55453354+Djoult@users.noreply.github.com>
2023-09-03 17:12:58 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/project-Dream4IT">
           <Global styles={GlobalStyles} />
           <App />
         </BrowserRouter>
