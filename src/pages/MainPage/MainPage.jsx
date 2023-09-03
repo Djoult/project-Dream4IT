a9ee435b92cfac143a7536c376e2442f508c6a3d
Djoult <55453354+Djoult@users.noreply.github.com>
2023-09-03 12:11:56 +0300

    adding changes in main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,12 @@
 import HeroSection from "../../components/Hero/Hero";
+import PopularCategories from "../../components/PopularCategories/PopularCategories";
+import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/drinks");
+  };
   return (
     <>
       <HeroSection
@@ -9,6 +15,11 @@ const MainPage = () => {
         your one-stop destination for exploring, crafting, 
         and mastering the world's finest beverages. "
       ></HeroSection>
+      <PopularCategories categoryDrink="Ordinary Drink" />
+      <PopularCategories categoryDrink="Cocktail" />
+      <PopularCategories categoryDrink="Shake" />
+      <PopularCategories categoryDrink="Other/Unknown" />
+      <button onClick={handleButtonClick}>Other drinks</button>
     </>
   );
 };
