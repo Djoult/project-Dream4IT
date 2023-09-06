234ca630713e9e6da1d2a3d3250e5861c79e8715
Uliana Atanasova <kuv25ild@gmail.com>
2023-09-07 01:44:35 +0300

    added changes autologin

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,19 +1,34 @@
-import { useSelector } from "react-redux";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-import BackgroundLayout from "../../components/BackgroundLayout/BackgroundLayout";
-import { SigninForm } from "../../components/SigninForm/SigninForm";
+import BackgroundLayout from '../../components/BackgroundLayout/BackgroundLayout';
+import { SigninForm } from '../../components/SigninForm/SigninForm';
 
-import { CustomizedSnackbar } from "../../components/commonComponents/CustomizedSnackbar";
-import { selectIsLoading } from "../../redux/selectors";
+import { CustomizedSnackbar } from '../../components/commonComponents/CustomizedSnackbar';
+import { selectIsLoading } from '../../redux/selectors';
 
-import { SectionTitle } from "../WelcomePage/WelcomePage.styled";
-import { CustomLink, SigninWrapper } from "./SignInPage.styled";
+import { SectionTitle } from '../WelcomePage/WelcomePage.styled';
+import { CustomLink, SigninWrapper } from './SignInPage.styled';
+import { SET_TOKEN } from '../../redux/authSlice';
 
 export const SigninPage = () => {
-  const { error, isLoading } = useSelector((state) => ({
+  const { error, isLoading } = useSelector(state => ({
     error: state.auth.error,
     isLoading: selectIsLoading(state),
   }));
+  const { search } = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const params = new URLSearchParams(search);
+    const token = params.get('token');
+
+    if (token) {
+      dispatch({ type: SET_TOKEN, payload: { token } });
+    }
+  }, []);
+
   return (
     <BackgroundLayout>
       <SigninWrapper>
@@ -21,7 +36,7 @@ export const SigninPage = () => {
         <SigninForm isLoading={isLoading} />
         <CustomLink to="/signup">Registration</CustomLink>
       </SigninWrapper>
-      {error && <CustomizedSnackbar severity={"error"} message={error} />}
+      {error && <CustomizedSnackbar severity={'error'} message={error} />}
     </BackgroundLayout>
   );
 };
