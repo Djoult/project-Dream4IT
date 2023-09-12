77276079fe7ed3b717a74a334f76d6b81e30a02e
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-09-12 11:40:14 +0300

    done

--- src/redux/Cocktails/favorites-reduser.js
+++ src/redux/Cocktails/favorites-reduser.js
@@ -1,26 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import * as actions from "./cocktails-actions";
-import { fetchFavorites } from "./favorites-operations";
+import { fetchFavorites, deleteFavorite } from "./favorites-operations";
 
 const initialState = {
-  items: [
-    {
-      _id: 14523466,
-      drink: "Quentin",
-      instructions:
-        "In a shaker half-filled with ice cubes, combine the rum, Kahlua, and cream. Shake well. Strain into a cocktail glass and garnish with the nutmeg.",
-      drinkThumb:
-        "http://res.cloudinary.com/dec1shvoo/image/upload/v1689167161/cocktails-v1/drinks/Quentin.jpg",
-    },
-    {
-      _id: 257456688,
-      drink: "Irish Coffee",
-      instructions:
-        "Heat the coffee, whiskey and sugar; do not boil. Pour into glass and top with cream; serve hot.",
-      drinkThumb:
-        "http://res.cloudinary.com/dec1shvoo/image/upload/v1689167155/cocktails-v1/drinks/Irish_Coffee.jpg",
-    },
-  ],
+  items: [],
   loading: false,
   error: null,
 };
@@ -41,6 +23,19 @@ const favoritesSlice = createSlice({
             state.loading = false;
             state.error = payload;
         })
+        .addCase(deleteFavorite.pending, state => {
+          state.loading = true;
+          state.error = null;
+      })
+      .addCase(deleteFavorite.fulfilled, (state, {payload}) => {
+          state.loading = false;
+          const index = state.items.findIndex(item => item.id === payload);
+          state.items.splice(index, 1);
+      })
+      .addCase(deleteFavorite.rejected, (state, {payload}) => {
+          state.loading = false;
+          state.error = payload;
+      })
     }
 });
 
